423c6faadaafc6f60dad095bdd693788062706b7
Tor Storruste <tor@superhelt.org>
2020-10-18 13:13:48 +0200

    Added statistics

---
+++ Backing/ViewStatistics.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CardViewer.Model;
+using CardViewer.Service;
+using Microsoft.AspNetCore.Components;
+
+namespace CardViewer.Pages
+{
+    public partial class ViewStatistics : ComponentBase
+    {
+        [Inject]
+        public IRoundService RoundService { get; set; }
+
+        private List<Round> Rounds;
+
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                Rounds = await RoundService.GetRounds();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public IEnumerable<IGrouping<int, Round>> GroupRoundsByYear()
+        {
+            return Rounds.GroupBy(x => x.Date.Year).ToList().OrderBy(x => x.Key);
+        }
+
+        public IEnumerable<IGrouping<int, Round>> GroupRoundsByMonth()
+        {
+            return Rounds.GroupBy(x => x.Date.Month).ToList().OrderBy(x => x.Key);
+        }
+
+        public IEnumerable<IGrouping<System.DayOfWeek, Round>> GroupByDayOfWeek()
+        {
+            return Rounds.GroupBy(x => x.Date.DayOfWeek).ToList().OrderBy(x => x.Key);
+        }
+    }
+}
