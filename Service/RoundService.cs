99dff6a7efba6be7ec5199df930d2be4582a806d
Tor Storruste <tor@superhelt.org>
2020-10-18 13:52:51 +0200

    Changed from . to ∙

--- Service/RoundService.cs
+++ Service/RoundService.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using CardViewer.Model;
@@ -23,7 +24,7 @@ namespace CardViewer.Service
             result.EnsureSuccessStatusCode();
 
             var json = await result.Content.ReadAsStringAsync();
-            return JsonConvert.DeserializeObject<List<Round>>(json);
+            return JsonConvert.DeserializeObject<List<Round>>(json).OrderBy(x=>x.Date).Reverse().ToList();
         }
     }
 }
